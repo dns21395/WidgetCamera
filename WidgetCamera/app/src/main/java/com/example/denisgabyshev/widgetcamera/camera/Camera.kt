686e651df93a47b48ff74feec78034f527de1e81
dns21395 <dns21395@gmail.com>
2017-09-01 22:03:37 +0900

    adding camera

---
+++ WidgetCamera/app/src/main/java/com/example/denisgabyshev/widgetcamera/camera/Camera.kt
@@ -0,0 +1,23 @@
+package com.example.denisgabyshev.widgetcamera.camera
+
+import android.hardware.camera2.CameraManager
+
+/**
+ * Created by denisgabyshev on 01/09/2017.
+ */
+class Camera : CameraHelper {
+    private var cameraManager: CameraManager? = null
+
+    override fun getCamera() {
+        
+    }
+
+    override fun releaseCamera() {
+
+
+    }
+
+
+
+
+}
---
+++ WidgetCamera/app/src/main/java/com/example/denisgabyshev/widgetcamera/camera/CameraHelper.kt
@@ -0,0 +1,10 @@
+package com.example.denisgabyshev.widgetcamera.camera
+
+/**
+ * Created by denisgabyshev on 01/09/2017.
+ */
+interface CameraHelper {
+    fun getCamera()
+
+    fun releaseCamera()
+}
