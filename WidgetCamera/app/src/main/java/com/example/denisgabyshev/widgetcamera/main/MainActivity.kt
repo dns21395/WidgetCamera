a3ab665f21261752c899f797ae6dbb4048d2cacd
dns21395 <dns21395@gmail.com>
2017-09-03 15:26:12 +0900

    transparent status bar

--- WidgetCamera/app/src/main/java/com/example/denisgabyshev/widgetcamera/main/MainActivity.kt
+++ WidgetCamera/app/src/main/java/com/example/denisgabyshev/widgetcamera/main/MainActivity.kt
@@ -1,14 +1,49 @@
 package com.example.denisgabyshev.widgetcamera.main
 
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.View
 import com.example.denisgabyshev.widgetcamera.R
+import com.readystatesoftware.systembartint.SystemBarTintManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+
+
+    }
+
+    fun setupViews() {
+        setBackground()
+        transparentStatusBar()
+    }
+
+    fun setBackground() {
+        background.setImageDrawable(ContextCompat.getDrawable(applicationContext, R.drawable.background))
+    }
+
+    fun transparentStatusBar() {
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.statusBarColor = Color.TRANSPARENT
+
+            val tintManager = SystemBarTintManager(this)
+
+            // enable status bar tint
+            tintManager.isStatusBarTintEnabled = true
+            // enable navigation bar tint
+            tintManager.setNavigationBarTintEnabled(true)
+        }
+
+
 
     }
 
